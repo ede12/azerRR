97eb53552f92dc5cb1e0db58c9146d89b8e25be7
ede12 <projectunreal25@gmail.com>
2025-03-15 22:12:12 +0100

    erzezrtrrt
    rzezredqfddfreerrth

---
+++ Source/azer/Public/NewSkeletalMeshComponent.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/SkeletalMeshComponent.h"
+#include "NewSkeletalMeshComponent.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AZER_API UNewSkeletalMeshComponent : public USkeletalMeshComponent
+{
+	GENERATED_BODY()
+	
+};
